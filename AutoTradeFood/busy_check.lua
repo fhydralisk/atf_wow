d36f8c5001917b8457c1254de0c0b49c6c51501f
Hangyu Fan <fhy14@mails.tsinghua.edu.cn>
2020-01-06 00:30:48 +0800

    unknown

--- AutoTradeFood/busy_check.lua
+++ AutoTradeFood/busy_check.lua
@@ -7,11 +7,37 @@ local addonName, L = ...
 
 local busy_state_context = {
   ["samples"] = {
-    {["sample_ts"]=0, ["water"]=0, ["food"]=0}
+    {["sample_ts"]=0, ["water"]=0, ["bread"]=0}
   },
   ["is_busy"] = false,
-  ["threshold_busy_low"] = 8,
-  ["threshold_busy_high"] = 20,
-  ["sample_interval"] = 60,
-  ["sample_size"] = 5,
 }
+
+local threshold_busy_low = 8
+local threshold_busy_high = 20
+local sample_interval = 60
+local sample_size = 5
+
+
+local function get_last_sample()
+    return busy_state_context.samples[#busy_state_context.samples]
+end
+
+local function sample_busy()
+    if GetTime() - get_last_sample().sample_ts >= sample_interval then
+        table.insert(busy_state_context.samples, {
+            ["sample_ts"] = GetTime(),
+            ["water"] = L.F.get_water_count(),
+            ["bread"] = L.F.get_bread_count(),
+        })
+    end
+end
+
+local function cleanup_sample()
+    while #busy_state_context.samples > sample_size or (
+        busy_state_context.samples[1]
+        and
+        GetTime() - busy_state_context.samples[1].sample_ts > (sample_interval + 3) * sample_size
+    ) do
+        table.remove(busy_state_context.samples, 1)
+    end
+end
